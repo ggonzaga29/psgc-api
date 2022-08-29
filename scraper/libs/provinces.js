859f4bf553cb0f4d093197793dfe5376456e6486
Gian Gonzaga <gonzagagianfrancesco@gmail.com>
2022-08-30 02:06:02 +0800

    add province scraping

--- scraper/libs/provinces.js
+++ scraper/libs/provinces.js
@@ -5,6 +5,8 @@ const PROVINCE_BASE_URL =
 	'https://psa.gov.ph/classification/psgc/?q=psgc/provinces/';
 
 async function provinces(regions) {
+	const provinces = []
+
 	regions.forEach(async (region) => {
 		const { correspondenceCode } = region;
 
@@ -13,9 +15,23 @@ async function provinces(regions) {
 		const { data } = await axios.get(PROVINCE_URL);
 
 		const $ = cheerio.load(data);
-		const table = $("#classifytable").last();
+		const table = $('#classifytable').last();
+		const rows = $(table).children('tbody').children('tr');
 
-		debugger;
+		rows.each((index, el) => {
+			const provinceName = $(el).children('td:first-child').children('a').text();
+			const correspondenceCode = $(el).children("td:nth-child(3)").text()
+
+			const province = {
+				provinceName,
+				correspondenceCode,
+				regionCorrespondenceCode: region.correspondenceCode
+			}
+
+			provinces.push(province);
+		});
+
+		console.log(provinces);
 	});
 }
 
